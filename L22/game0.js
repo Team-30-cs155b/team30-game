889100a6345c153b023e0fd7cf03912a7666ddbb
Achroite404 <35772688+Achroite404@users.noreply.github.com>
2018-03-19 21:35:36 -0400

    Update game0.js

--- L22/game0.js
+++ L22/game0.js
@@ -510,8 +510,9 @@ The user moves a cube around the board trying to knock balls into a cone
 			case "ArrowRight": avatarCam.translateY(-1);break;
 			case "ArrowUp": avatarCam.translateZ(-1);break;
 			case "ArrowDown": avatarCam.translateZ(1);break;
-			case "q": avatarCam.rotateY(-1); break;
-			case "e": avatarCam.rotateY(1); break;
+			case "q": avatarCam.rotateY(Math.PI/180); break;
+			case "e": avatarCam.rotateY(-Math.PI/180); break;
+			case "b": avatarCam.rotation.y = 0;
 
 		}
 
